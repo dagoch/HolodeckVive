9ee81bcf8a1da9620a0a4884796626a6237297ca
Wenbo Lan <info@wenbolan.com>
2017-04-02 18:17:33 -0400

    refactor the scene, add scenechange function, ready to build

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FRL.IO;
+using System;
+
+public class SceneChanger : MonoBehaviour, IGlobalApplicationMenuPressDownHandler {
+
+    public void OnGlobalApplicationMenuPressDown(ViveControllerModule.EventData eventData)
+    {
+        index = (index + 1) % 4;
+        changemodel();
+    }
+
+    public GameObject Stage;
+    public GameObject Lab;
+    public GameObject LaScala;
+    public GameObject PlayArea;
+
+    [SerializeField] private int index;
+
+    private void changemodel()
+    {
+        if(index == 0)
+        {
+            Stage.SetActive(false);
+            Lab.SetActive(true);
+            LaScala.SetActive(false);
+            PlayArea.SetActive(false);
+        }
+
+        if (index == 1)
+        {
+            Stage.SetActive(false);
+            Lab.SetActive(true);
+            LaScala.SetActive(false);
+            PlayArea.SetActive(true);
+        }
+
+        if (index == 2)
+        {
+            Stage.SetActive(true);
+            Lab.SetActive(true);
+            LaScala.SetActive(false);
+            PlayArea.SetActive(false);
+        }
+        if (index == 3)
+        {
+            Stage.SetActive(false);
+            Lab.SetActive(false);
+            LaScala.SetActive(true);
+            PlayArea.SetActive(false);
+        }
+    }
+    // Use this for initialization
+    void Start () {
+        index = 0;
+        changemodel();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
