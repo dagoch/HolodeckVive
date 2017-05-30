05f971f3e1b1a6fb13f89deb422b2da23a0f9287
Owen <owenbellgames@gmail.com>
2017-05-30 16:27:33 -0400

    Setup SyncedScaleTest scene for handling for first test

--- Assets/Scripts/ScaleEnvironment.cs
+++ Assets/Scripts/ScaleEnvironment.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FRL.IO;
+using Holojam.Tools;
+using Holojam.Vive;
 
-public class ScaleEnvironment : MonoBehaviour {
+public class ScaleEnvironment : Synchronizable, IGlobalTriggerPressDownHandler, IGlobalTriggerPressUpHandler {
 
+    public Transform HeadTransform;
     public Transform HolojamTransform;
     public Transform EnvironmentTransform;
     public float ScaleFactor;
-    public Transform TestTransform;
 
     private float _StartY;
     private bool _TriggerDown;
     private Vector3 _StartScale;
     private float[] _SavedLightRanges;
+
+    private ViveControllerReceiver _VCReceiver;
+
+    public override bool Host {
+        get {
+            if (!_VCReceiver) _VCReceiver = GetComponent<ViveControllerReceiver>();
+            return _VCReceiver.index == Holojam.Tools.BuildManager.BUILD_INDEX;
+        }
+    }
+
+    public override bool AutoHost { get { return false; } }
+
+    public override string Label {
+        get {
+            return this.name + "-environmentScaler";
+        }
+    }
 
 	// Use this for initialization
 	void Start () {
@@ -21,7 +41,51 @@ public class ScaleEnvironment : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    if (Input.GetKeyDown(KeyCode.B) && !_TriggerDown) {
+	    //if (Input.GetKeyDown(KeyCode.B) && !_TriggerDown) {
+     //       _TriggerDown = true;
+     //       _StartY = transform.position.y;
+     //       _StartScale = EnvironmentTransform.localScale;
+     //       var lights = EnvironmentTransform.GetComponentsInChildren<Light>();
+     //       _SavedLightRanges = new float[lights.Length];
+     //       for (var i = 0; i < lights.Length; i++) {
+     //           _SavedLightRanges[i] = lights[i].range;
+     //       }
+     //   }	
+     //   if (Input.GetKeyUp(KeyCode.B) && _TriggerDown) {
+     //       _TriggerDown = false;
+     //   }
+
+        //if (_TriggerDown) {
+        //    var diff = HeadTransform.position.y - _StartY;
+        //    var scale = _StartScale * (1 + diff * ScaleFactor);
+        //    EnvironmentTransform.localScale = scale;
+        //    var lights = EnvironmentTransform.GetComponentsInChildren<Light>();
+        //    for (var i = 0; i < lights.Length; i++) {
+        //        lights[i].range = _SavedLightRanges[i] * (1 + diff * ScaleFactor);
+        //    }
+        //}
+	}
+
+    public override void ResetData() {
+        data = new Holojam.Network.Flake(1, 0, 0, 0);
+    }
+
+    protected override void Sync() {
+        if (Host) {
+            if (_TriggerDown) {
+                var diff = HeadTransform.position.y - _StartY;
+                var scale = _StartScale * (1 + diff * ScaleFactor);
+                EnvironmentTransform.localScale = scale;
+            }
+            data.vector3s[0] = EnvironmentTransform.localScale;
+        }
+        else {
+            EnvironmentTransform.localScale = data.vector3s[0];
+        }
+    }
+
+    void IGlobalTriggerPressDownHandler.OnGlobalTriggerPressDown(VREventData eventData) {
+        if (Host) {
             _TriggerDown = true;
             _StartY = transform.position.y;
             _StartScale = EnvironmentTransform.localScale;
@@ -30,21 +94,12 @@ public class ScaleEnvironment : MonoBehaviour {
             for (var i = 0; i < lights.Length; i++) {
                 _SavedLightRanges[i] = lights[i].range;
             }
-        }	
-        if (Input.GetKeyUp(KeyCode.B) && _TriggerDown) {
+        }
+    }
+
+    void IGlobalTriggerPressUpHandler.OnGlobalTriggerPressUp(VREventData eventData) {
+        if (Host) {
             _TriggerDown = false;
         }
-
-        if (_TriggerDown) {
-            var posDiff = EnvironmentTransform.position - HolojamTransform.position;
-            Debug.Log(posDiff);
-            var diff = transform.position.y - _StartY;
-            var scale = _StartScale * (1 + diff * ScaleFactor);
-            EnvironmentTransform.localScale = scale;
-            var lights = EnvironmentTransform.GetComponentsInChildren<Light>();
-            for (var i = 0; i < lights.Length; i++) {
-                lights[i].range = _SavedLightRanges[i] * (1 + diff * ScaleFactor);
-            }
-        }
-	}
+    }
 }
--- Assets/Scripts/ScaleSizeWithHeight.cs
+++ Assets/Scripts/ScaleSizeWithHeight.cs
@@ -1,8 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FRL.IO;
+using Holojam.Tools;
+using Holojam.Vive;
+using System;
 
-public class ScaleSizeWithHeight : MonoBehaviour {
+public class ScaleSizeWithHeight : Synchronizable, IGlobalTriggerPressDownHandler, IGlobalTriggerPressUpHandler {
+
+    public Transform HeadTransform;
+    public Transform LeftHandTransform;
+    public Transform RightHandTransform;
 
     [Range(0, 1)]
     public float LowerLimit;
@@ -10,24 +18,73 @@ public class ScaleSizeWithHeight : MonoBehaviour {
     private float _StartY;
     private bool _TriggerDown;
 
-	// Use this for initialization
-	void Start () {
+    private ViveControllerReceiver _VCReceiver;
+
+    public override bool Host {
+        get {
+            if (!_VCReceiver) _VCReceiver = GetComponent<ViveControllerReceiver>();
+            return _VCReceiver.index == Holojam.Tools.BuildManager.BUILD_INDEX;
+        }
+    }
+
+    public override bool AutoHost { get { return false; } }
+
+    public override string Label {
+        get {
+            return this.name + "-heightScaler";
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Space) && !_TriggerDown) {
-            _TriggerDown = true;
-            _StartY = transform.position.y;
+        //if (Input.GetKeyDown(KeyCode.Space) && !_TriggerDown) {
+        //    _TriggerDown = true;
+        //    _StartY = transform.position.y;
+        //}
+        //else if (Input.GetKeyUp(KeyCode.Space) && _TriggerDown) {
+        //    _TriggerDown = false;
+        //}
+
+        //if (_TriggerDown) {
+        //    var percent = Mathf.Clamp(HeadTransform.position.y / _StartY, LowerLimit, 1f);
+        //    HeadTransform.localScale = Vector3.one * percent;
+        //}
+	}
+
+    public override void ResetData() {
+        data = new Holojam.Network.Flake(1, 0, 0, 0);
+    }
+
+    protected override void Sync() {
+        if (Host) {
+            if (_TriggerDown) {
+                var percent = Mathf.Clamp(HeadTransform.position.y / _StartY, LowerLimit, 1f);
+                HeadTransform.localScale = Vector3.one * percent;
+            }
+            data.vector3s[0] = HeadTransform.localScale;
+            //data.ints[0] = (_TriggerDown) ? 1 : 0;
         }
-        else if (Input.GetKeyUp(KeyCode.Space) && _TriggerDown) {
+        else {
+            HeadTransform.localScale = data.vector3s[0];
+            //_TriggerDown = (data.ints[0] == 1) ? true : false;
+        }
+    }
+
+    void IGlobalTriggerPressDownHandler.OnGlobalTriggerPressDown(VREventData eventData) {
+        if (Host) {
+            _TriggerDown = true;
+            _StartY = HeadTransform.position.y;
+        }
+    }
+
+    void IGlobalTriggerPressUpHandler.OnGlobalTriggerPressUp(VREventData eventData) {
+        if (Host) {
             _TriggerDown = false;
         }
-
-        if (_TriggerDown) {
-            var percent = Mathf.Clamp(transform.position.y / _StartY, LowerLimit, 1f);
-            transform.localScale = Vector3.one * percent;
-        }
-	}
+    }
 }
