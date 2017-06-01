8be547f99cde1c950f98b50d76a83aa42046cf5e
Owen <owenbellgames@gmail.com>
2017-06-01 17:00:38 -0400

    Animations with interaction probe and colliders work

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class DoorController : MonoBehaviour {
 
-	// Use this for initialization
+    public TriggerSubscriber OpenSubscriber;
+    public TriggerSubscriber CloseSubscriber;
+    public float InteractionLockOut;
+
+    private Animator _Animator;
+    private float _RemainingLockout;
+
 	void Start () {
-		
+        _Animator = GetComponent<Animator>();
+
+        OpenSubscriber.SubscribeToEnter(OnOpenEnter);
+        CloseSubscriber.SubscribeToEnter(OnCloseEnter);
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+        _RemainingLockout -= Time.deltaTime;	
 	}
+
+    void OnOpenEnter(Collider other) {
+        if (_RemainingLockout > 0f) {
+            return;
+        }
+        _RemainingLockout = InteractionLockOut;
+        _Animator.SetTrigger("Open");
+    }
+
+    void OnCloseEnter(Collider other) {
+        if (_RemainingLockout > 0f) {
+            return;
+        }
+        _RemainingLockout = InteractionLockOut;
+        _Animator.SetTrigger("Close");
+    }
 }
