caefe536a9c234fd6b5c299ec5eb0170bf35859b
snowymo <snowymohehe@gmail.com>
2017-06-03 20:52:15 -0400

    working on appear/disappear scripts

---
+++ Assets/Scripts/AppearAndDisappear_Rotation.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearAndDisappear_Rotation : MonoBehaviour {
+  //object will appear when the scene begins
+  public bool appearOnStart;
+  //if object is below the user - this is needed because of the way unity handles head rotation 
+  //and the way I'm checking it
+  public bool below;
+  //seconds after scene is loaded at which objects will appear or disappear
+  public float time;
+  //angle distance up or down from straight forward (0 = straight forward)
+  //  which will trigger appearance or disappearance of object.  Always positive degrees off axis.
+  public float rotation;
+  //user camera
+  public Camera cr;
+
+  bool seen = true;
+  MeshRenderer mr;
+
+  void Start()
+  {
+    mr = GetComponent<MeshRenderer>();
+    if (!appearOnStart)
+    {
+      mr.enabled = false;
+    }
+    //debug
+    //StartCoroutine(StartTimer(false));
+  }
+
+  // Call this to reset object to initial state
+  void reset()
+  {
+    switchState(appearOnStart);
+  }
+
+  // Immediately toggle visibility of object (e.g. for reset)
+  //state = true will turn all associated meshrenderers on, and vice versa
+  void switchState(bool state)
+  {
+    Debug.Log("switching");
+    if (mr)
+    {
+      mr.enabled = state;
+    }
+    foreach (MeshRenderer mesh in GetComponentsInChildren<MeshRenderer>())
+    {
+      mesh.enabled = state;
+    }
+  }
+
+  public void StartTimerToggle()
+  {
+    StartCoroutine(StartTimer(!appearOnStart));
+  }
+
+
+  // Call this to start timer; when timer expires, start checking head tilt every frame
+  //  and if head tilted past threshold in correct direction, change visibility based on 
+  //  state flag
+  //state = true will turn all associated meshrenderers on, and vice versa
+  // to toggle from initial state, call this with !appearOnStart
+  public IEnumerator StartTimer(bool state)
+  {
+    yield return new WaitForSeconds(time);
+    seen = true;
+    while (seen)
+    {
+      float currentRotation = cr.transform.rotation.eulerAngles.x;
+      //if we're below the user, we check the opposite angle (e.g. 330 degrees becomes 30, and vice versa)
+      if (below)
+      {
+        currentRotation = 360.0f - currentRotation;
+      }
+      //Debug.Log(currentRotation + " " + rotation);
+      if (currentRotation > rotation && currentRotation < 180)
+      {
+        switchState(state);
+        seen = false;
+      }
+      yield return null;
+    }
+  }
+}
---
+++ Assets/Scripts/AppearAndDisappear_View.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearAndDisappear_View : MonoBehaviour {
+  //object will appear when the scene begins
+  public bool appearOnStart;
+  //seconds after scene is loaded at which objects will appear or disappear
+  public float time;
+  //choose to use viewport checks or, if not, render checks to see if an object is "invisible"
+  public bool checkViewport = true; // always this, except for making room pieces disappear
+  //user camera
+  public Camera cr;
+  //if useViewport, this var will matter, else, it doesn't
+  // how much additional distance outside viewport should object's center be before you consider it
+  //  unseen.  (usually between 0 and 2.   viewport coordinates measure from 0 - 1 on each axis)
+  public float viewportBuffer;
+
+  bool seen = true;
+  bool invisible;
+  MeshRenderer mr;
+
+  void Start()
+  {
+    mr = GetComponent<MeshRenderer>();
+    if (!appearOnStart)
+    {
+      mr.enabled = false;
+    }
+    //debug
+//    StartCoroutine(StartTimer(true));
+  }
+
+  // Call this to reset object to initial state
+  void reset()
+  {
+    switchState(appearOnStart);
+  }
+
+  // This immediately turns object on or off
+  //state = true will turn all associated meshrenderers on, and vice versa
+  void switchState(bool state)
+  {
+    if (mr)
+    {
+      mr.enabled = state;
+    }
+    foreach (MeshRenderer mesh in GetComponentsInChildren<MeshRenderer>())
+    {
+      mesh.enabled = state;
+    }
+  }
+
+  // Note: these will never be called if the meshrenderer is off!
+  void OnBecameInvisible()
+  {
+    Debug.Log("Now you don't.");
+    invisible = true;
+  }
+
+  void OnBecameVisible()
+  {
+    Debug.Log("Now you see me.");
+    invisible = false;
+  }
+
+  // Call this to start timer; when timer expires, start checking if object is outside of 
+  //  viewport (or is invisible to renderer), and if so, change visibility based on 
+  //  state flag
+  //state = true will turn all associated meshrenderers on, and vice versa
+  public IEnumerator StartTimer(bool state)
+  {
+    yield return new WaitForSeconds(time);
+    seen = true;
+    while (seen)
+    {
+      if(checkViewport)
+      {
+        //checking a numerical graph of the camera viewport (x/y 0 to 1, + or - viewportBuffer) to see if the object's transform is there
+        //this will often allow objects whose models go way beyond their transforms to pop into view
+        Vector3 screenPoint = cr.WorldToViewportPoint(transform.position);
+        bool onScreen = screenPoint.z > 0 - viewportBuffer && screenPoint.x > 0 - viewportBuffer && screenPoint.x < 1 + viewportBuffer && screenPoint.y > 0 - viewportBuffer && screenPoint.y < 1 + viewportBuffer;
+        if (!onScreen)
+        {
+          seen = false;
+          switchState(state);
+        }
+      //}  disabled for now because only useful when making large objects disappear; won't ever work if it's already invisible
+      //else
+      //{
+      //  //invisible bool handled by separate OnBecameVisible/Invisible methods
+      //  if (invisible)
+      //  {
+      //    seen = false;
+      //    switchState(state);
+      //  }
+      }
+      yield return null;
+    }
+   
+  }
+}
