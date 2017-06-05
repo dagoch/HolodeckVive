4eeb6b4f0ce69119f501bbee45a5332b718b84db
Owen <owenbellgames@gmail.com>
2017-06-05 19:38:34 -0400

    Basic grabbing and synced props should be working together

--- Assets/InputSystems-master/Examples/Grab/Grabber.cs
+++ Assets/InputSystems-master/Examples/Grab/Grabber.cs
@@ -1,6 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using FRL.IO;
 
-public class Grabber : MonoBehaviour {
+public class Grabber : MonoBehaviour, IGlobalTriggerPressDownHandler, IGlobalTriggerPressUpHandler {
 
+    private GameObject _OverlappedObject;
+    private bool _Holding = false;
+    private Transform _SavedTransform;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Grabbable") {
+            _OverlappedObject = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+       if (other.gameObject == _OverlappedObject && !_Holding) {
+            _OverlappedObject = null;
+        } 
+    }
+
+    void IGlobalTriggerPressDownHandler.OnGlobalTriggerPressDown(VREventData eventData) {
+        if (_OverlappedObject != null) {
+            _SavedTransform = _OverlappedObject.transform.parent;
+            _OverlappedObject.transform.parent = transform;
+            Debug.Log("set to new parent: " + transform);
+            _Holding = true;
+        }
+    }
+
+    void IGlobalTriggerPressUpHandler.OnGlobalTriggerPressUp(VREventData eventData) {
+        if (_OverlappedObject != null) {
+            _Holding = false;
+            Debug.Log("set to old parent: " + _SavedTransform);
+            _OverlappedObject.transform.parent = _SavedTransform;
+        }
+    }
 }
